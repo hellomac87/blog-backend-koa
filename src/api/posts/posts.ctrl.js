a042337de6f52323e4a2c583cc0ba819392a0a0a
hellomac87 <hellomac87@gmail.com>
2018-09-10 19:48:08 +0900

    데이터 삭제와 수정

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -43,17 +43,45 @@ exports.read = async(ctx) => {
         }
 
         ctx.body = post;
-        
+
     }catch(e){
         ctx.throw(e, 500);
     }
     
 }
+/* DELETE /api/posts/:id
+*/
+exports.remove = async(ctx) => {
+    const { id } = ctx.params;
 
-exports.remove = (ctx) => {
-    
+    try{
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204;
+    }catch(e){
+        ctx.throw(e, 500);
+    }
 };
 
-exports.update = (ctx) => {
-    
+/* PATCH /api/posts/:id
+{ title, body, tags }
+*/
+exports.update = async(ctx) => {
+    const { id } = ctx.params;
+
+    try{
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true
+            // 이 값을 설정해야 업데이트된 객체를 반환합니다.
+            // 설정하지 않으면 업데이트되기 전의 객체를 반환합니다.
+        }).exec();
+
+        // 포스트가 존재하지 않을 때
+        if(!post){
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    }catch(e){
+        ctx.throw(e, 500);
+    }
 };
