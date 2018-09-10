734c21f5a98c57460aed130fb4c3a7d6df3fb346
hellomac87 <hellomac87@gmail.com>
2018-09-10 20:12:16 +0900

    마지막 페이지 번호 알려 주기

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -69,6 +69,11 @@ exports.list = async(ctx) => {
             .limit(10)
             .skip((page - 1) * 10)
             .exec();
+        
+        const postCount = await Post.count().exec();
+        // 마지막 페이지 알려 주기
+        // ctx.set 은 request header 를 설정
+        ctx.set('Last-Page', Math.ceil(postCount / 10));
                                 
         ctx.body = posts;
     }catch(e){
