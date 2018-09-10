8b8a4591c6d1ef35f8e1bfc7dd6e08f33c4f7377
hellomac87 <hellomac87@gmail.com>
2018-09-10 19:52:14 +0900

    요청 검증 - ObjectId

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,5 +1,16 @@
 const Post = require("models/post");
+const { ObjectId } = require("mongoose").Types;
 
+exports.checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+
+    // 검증 실패
+    if(!ObjectId.isValid(id)){
+        ctx.status = 400; // 400 Bad Request
+        return null;
+    }
+    return next(); // next를 리턴해야 ctx.body 가 제대로 설정됩니다.
+}
 /* POST /api/posts
 { title, body, tags }
 */
