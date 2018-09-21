d2eccec65a939b86bcaf05865a69a7317dbfa3fe
hellomac87 <hellomac87@gmail.com>
2018-09-21 17:41:07 +0900

    서버에 세션 적용

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-body-parser');
 const mongoose = require('mongoose');
+const session = require('koa-session');
 
 const api = require('./api');
 
 const {
     PORT : port = 4000, //값이 존재하지 않는다면 4000 을 기본 값으로 사용
     MONGO_URI : mongoURI,
+    COOKIE_SIGN_KEY : signKey
 } = process.env;
 
 mongoose.Promise = global.Promise; // Node의 Promise를 사용하도록 설정
@@ -28,8 +30,14 @@ router.use('/api', api.routes());
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
 
+// 세션/키 적용
+const sessionConfig = {
+    maxAge : 86400000, // 하루
+    // signed: true
+}
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+app.keys = [signKey];
 
 app.listen(port, () => {
     console.log('listening to port',port);
