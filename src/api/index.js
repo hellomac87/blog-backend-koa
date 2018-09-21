5fc98236d0f68d4aacfd7c36ad669604bd95b0d3
hellomac87 <hellomac87@gmail.com>
2018-09-21 17:54:03 +0900

    api 라우트에 auth 적용

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router');
 const posts = require('./posts');
+const auth = require('./auth');
 
 const api = Router();
 
 api.use('/posts', posts.routes());
+api.use("/auth", auth.routes());
 
 // ㄹㅏ우트를 내보냅니다.
 module.exports = api;
